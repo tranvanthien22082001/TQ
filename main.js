08b2c4c02f3c022a8871e6f91310b70227ebbcce
Your Name <you@example.com>
2023-01-03 17:53:05 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+const sr = ScrollReveal ({
+	distance: '65px',
+	duration: 2600,
+	delay: 450,
+	reset: true
+});
+
+sr.reveal('.text',{delay:200, origin:'left'});
+sr.reveal('.home-bg',{delay:450, origin:'top'});
+sr.reveal('.box',{delay:450, origin:'top'});
